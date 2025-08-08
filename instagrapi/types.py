3c61a0d2c897781069d329560a1d305dc7b82d8a
Ekin Dursun <ekindursun@gmail.com>
2025-08-09 00:02:22 +0300

    Additional fixes

--- instagrapi/types.py
+++ instagrapi/types.py
@@ -360,8 +360,8 @@ class ClipsMetadata(TypesBaseModel):
     achievements_info: ClipsAchievementsInfo
     additional_audio_info: ClipsAdditionalAudioInfo
     asset_recommendation_info: Optional[dict] = None
-    audio_ranking_info: ClipsAudioRankingInfo
-    audio_type: str = "original_sounds"
+    audio_ranking_info: ClipsAudioRankingInfo | None
+    audio_type: str | None = "original_sounds"
     branded_content_tag_info: ClipsBrandedContentTagInfo
     breaking_content_info: Optional[dict] = None
     breaking_creator_info: Optional[dict] = None
